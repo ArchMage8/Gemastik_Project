f63b3e480d07e5746609c11f63772111cc0d279b
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-05-31 10:05:29 +0700

    Blind Animation Setup

--- Assets/GameProject/Scripts/Characters/BlindPersonAnimation.cs
+++ Assets/GameProject/Scripts/Characters/BlindPersonAnimation.cs
@@ -6,31 +6,43 @@ public class BlindPersonAnimation : MonoBehaviour
 {
     public  SimulationManager simulationManager;
     private float directionFloat;
+    private Animator animator;
     private bool isMoving;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         simulationManager.directionFloat = directionFloat;
         simulationManager.isMoving = isMoving;
 
+        animator.SetBool("IsMoving", isMoving);
+
         if (directionFloat == 5f)   //NorthEast
         {
-
+            animator.SetFloat("Vertical", 1);
+            animator.SetFloat("Horizontal", 1);
         }
 
         else if (directionFloat == 6f)  //NorthWest
         {
-
+            animator.SetFloat("Vertical", -1);
+            animator.SetFloat("Horizontal", 1);
         }
 
         else if (directionFloat == 7f)  //SouthEast
         {
-
+            animator.SetFloat("Vertical", 1);
+            animator.SetFloat("Horizontal", -1);
         }
 
         else if (directionFloat == 8f)  //SouthWest
         {
-
+            animator.SetFloat("Vertical", -1);
+            animator.SetFloat("Horizontal", 1);
         }
     }
 }
