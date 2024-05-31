acca104952e48896466e3363fe5365d28541471d
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-05-31 14:14:17 +0700

    buttons draft

---
+++ Assets/GameProject/Scripts/InGameButtons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameButtons : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private SimulationManager simulationManager;
+    [SerializeField] private int HomeSceneIndex;
+    [SerializeField] private Sprite ActivatedSprite;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartSim()
+    {
+        playerController.enabled = false;
+        simulationManager.isSimulating = true;
+
+        GetComponent<SpriteRenderer>().sprite = ActivatedSprite;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(HomeSceneIndex);
+    }
+}
