7e01a24315fbee0d6cadc69653b4bb408e5dde20
Hanhan <52540618+HanhanPx@users.noreply.github.com>
2024-06-14 16:55:09 +0700

    nice boat bro

--- Assets/GameProject/Scripts/ObjectGenerator.cs
+++ Assets/GameProject/Scripts/ObjectGenerator.cs
@@ -7,16 +7,17 @@ public class ObjectGenerator : MonoBehaviour
     public GameObject prefab;
     public GameObject destination;
     public float timeDelay = 15f;
-    private int speed;
-    [SerializeField] private int min_speed;
-    [SerializeField] private int max_speed;
+    public float StartDelay = 5f;
+    private float speed;
+    [SerializeField] private float min_speed;
+    [SerializeField] private float max_speed;
     public float fadeRate;
 
     private bool canFadeOut = true;
 
     void Start()
     {
-        StartCoroutine(ObjectGeneration());
+        StartCoroutine(StartSystem());
     }
 
     private void Update()
@@ -119,5 +120,11 @@ public class ObjectGenerator : MonoBehaviour
     private void RandomizerSpeed()
     {
         speed = Random.Range(min_speed, max_speed);
+    }
+
+    private IEnumerator StartSystem(){
+
+        yield return new WaitForSeconds(StartDelay);
+        StartCoroutine(ObjectGeneration());
     }
 }
