62b1fe444d18e425672d93e1059993005c44a8ad
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-06-26 14:19:20 +0700

    animation scripting done

--- Assets/GameProject/Wordel/Scripts/WinHandler.cs
+++ Assets/GameProject/Wordel/Scripts/WinHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinHandler : MonoBehaviour
 {
@@ -8,35 +10,72 @@ public class WinHandler : MonoBehaviour
 
     [Space(10)]
     [Header("Animator: ")]
-    private Animator animator;
-    private bool StartBool = false;
+    public Animator Bus;
+    public Animator LeftDoor;
+    public Animator RightDoor;
+    public Animator Peoples;
 
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
+    [Header("Level Loader: ")]
+    public Animator LevelTrigger;
+    public int destinationScene;
+    [SerializeField] private AudioSource BGMusic;
+
+    //Sequence:
+    // Bus Arrives          -> Bus
+    // Open Bus Doors       -> Bus
+    // Open Station Doors   -> Doors
+    // Character walk cycle -> People
+
     public void Complete()
     {
-        StartBool = true;
+        StartCoroutine(BusAnims());
     }
 
-    public void Bus()
+    private IEnumerator BusAnims()
     {
-        if (StartBool)
+        Bus.SetTrigger("BusEntry");
+        yield return new WaitForSeconds(1);
+
+        Bus.SetTrigger("OpenDoors"); //Bus Doors
+        
+    }
+
+    public void OpenStationDoors()
+    {
+        LeftDoor.SetTrigger("OpenDoor");
+        RightDoor.SetTrigger("OpenDoor");
+    }
+
+    public void MovePeoples()
+    {
+        Peoples.SetTrigger("MovePeople");
+    }
+
+    private IEnumerator Toggler()
+    {
+        StartCoroutine(FadeOutMusic());
+        LevelTrigger.SetTrigger("Start");
+        yield return new WaitForSeconds(1.2f);
+
+        
+            SceneManager.LoadScene(destinationScene);
+        
+
+       
+
+    }
+
+    private IEnumerator FadeOutMusic()
+    {
+        float startVolume = BGMusic.volume;
+
+        for (float t = 0; t < 1.2f; t += Time.deltaTime)
         {
-            animator.SetTrigger("Bus"); //Bus Arrives
+            BGMusic.volume = Mathf.Lerp(startVolume, 0, t / 1.2f);
+            yield return null;
         }
+
+        BGMusic.volume = 0;
     }
 
-    public void Doors()
-    {
-        animator.SetTrigger("Doors"); //Open Doors
-    }
-
-    public void Peoples()
-    {
-        animator.SetTrigger("People"); //Walk Cycles
-    }
-
-    
 }
